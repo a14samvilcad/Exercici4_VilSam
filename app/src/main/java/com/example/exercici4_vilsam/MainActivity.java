0bbc47529744a92a58f27dce1c3045eab941d8fd
a14samvilcad <a14samvilcad@inspedralbes.cat>
2022-09-21 18:02:46 +0200

    Fet

--- app/src/main/java/com/example/exercici4_vilsam/MainActivity.java
+++ app/src/main/java/com/example/exercici4_vilsam/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.exercici4_vilsam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int contador = 0;
+    private TextView mostrarContador;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mostrarContador = findViewById(R.id.lblCompte);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        contador++;
+        if(mostrarContador != null){
+            mostrarContador.setText(Integer.toString(contador));
+        }
     }
 }
